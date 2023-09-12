976f3785bb793ea7de0114c8b248e9bf4d24aa49
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-12 22:34:04 +0200

    working on milestone 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* 
 Descrizione
 Ricreiamo un feed social aggiungendo al layout dello starter kit di base fornito, il nostro script JS in cui:
+
 Milestone 1
 Creiamo il nostro array di oggetti che rappresentano ciascun post.
 Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
@@ -13,10 +14,13 @@ immagine (non tutti i post devono avere una immagine),
 numero di likes.
 Non è necessario creare date casuali
 Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+
 Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
 Milestone 3
 Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+
 BONUS
 Formattare le date in formato italiano (gg/mm/aaaa)
 Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
@@ -28,10 +32,7 @@ Poiché é la parte logica ad interessarci in questa fase del corso, nello start
 Se finite la parte logica ed i vari bonus e vi avanza tempo per giocare un pó, pensate pure ad un layout differente e lavorateci su come bonus extra.
 */
 
-/***************************
-    SET UP VARIABLES
-****************************/
-const container = document.getElementById('container');
+const containerElement = document.getElementById('container');
 
 // post a cui l'utente ha già messo like
 const userLikes = [2, 3, 6];
@@ -94,35 +95,27 @@ const posts = [
     }              
 ];
 
-
-
-/***************************
-    Render dei post
-****************************/
 for (let i = 0; i < posts.length; i++) {
     const currentPost = posts[i];
-    container.innerHTML += postTemplate(currentPost);
+    //containerElement.innerHTML += postTemplate(currentPost);
+    //console.log(currentPost.likes);
+
+    containerElement.innerHTML += postMarkup(currentPost)
 }
 
 
-
-/***************************
-    FUNCTIONS
-****************************/
-// funnzione per generare la card di un post
-function postTemplate(postData) {
+function postMarkup(postData) {
     const { id, author, content, created, media, likes } = postData;
     return `
     <div class="post">
         <div class="post__header">
             <div class="post-meta">                    
                 <div class="post-meta__icon">
-                    ${author.image ? profileImageTemplate(author) : profileImageDefaultTemplate(author)}         
-
+                    <img class="profile-pic" src="${media}" alt="Phil Mangione"> 
                 </div>
                 <div class="post-meta__data">
                     <div class="post-meta__author">${author.name}</div>
-                    <div class="post-meta__time">${formatDate(created)}</div>
+                    <div class="post-meta__time">4 mesi fa</div>
                 </div>                    
             </div>
         </div>
@@ -133,106 +126,18 @@ function postTemplate(postData) {
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button ${isPostLiked(id) ? 'like-button--liked' : ''} js-like-button" href="#" data-postid="${id}">
+                    <a class="like-button js-like-button" href="javascript:;" data-postid="${id}">
                         <i class="like-button__icon fas fa-thumbs-up"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-${id}" class="js-likes-counter">${likes}</b> persone
+                    Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
                 </div>
             </div> 
         </div>            
     </div>`;
 }
 
-// funzione per stampare l'immagine profilo (facoltativa, utile per fare bonus 2)
-function profileImageTemplate (userData) {
-    const { name, image } = userData;
-    return `<img class="profile-pic" src="${image}" alt="${name}">`;
-}
-
-// Bonus 2: funzione di fallback immagine profilo
-function profileImageDefaultTemplate (userData) {
-    const { name } = userData;
-
-    // ricaviamo le iniziali del nome
-    const nameParts = name.split(' ');
-
-    const letters = [];
-    for (let i = 0; i < nameParts.length; i++) {
-        const namePart = nameParts[i];
-        const initialLetter = namePart[0]; // oppure namePart.charAt(0);
-        letters.push(initialLetter);
-    }
-    console.log(letters);
-    const initials = letters.join('');
-
-    return `
-        <div class="profile-pic-default">
-            <span>${initials}</span>
-        </div> 
-    `;
-}
-
-// BONUS 1: visualizzare la data in formato italiano
-function formatDate(dateStr) {
-    console.log(dateStr);
-    console.log(dateStr.split('-').reverse().join('/'));
-    return dateStr.split('-').reverse().join('/');
-}
-
-// funzione usata nel ternario per aggiungere la classe "liked" al bottone del post (BONUS)
-function isPostLiked(postId) {
-    return userLikes.includes(postId);
-}
-
-
-
-/***************************
-    BONUS 3: Like Button
-****************************/
-// ricordiamo che querySelector() restituisce il PRIMO elemento del DOM che corrisponde alla selezione
-// per recuperare TUTTI gli elementi che hanno la stessa classe usiamo querySelectorAll()
-const likeButtons = document.querySelectorAll('.js-like-button');
-console.log(likeButtons);
-const likeCounters = document.querySelectorAll('.js-likes-counter');
-
-// aggiungiamo ad ogni pulsante l'event listener
-for (let i = 0; i < likeButtons.length; i++) {
-    const element = likeButtons[i];
-    element.addEventListener('click', function (e){
-        // se usiamo tag a mostriamo preventDefault(), altrimenti possiamo usare tag button
-        e.preventDefault();
-
-        if (!element.classList.contains('like-button--liked')) {
-
-            element.classList.add('like-button--liked');
-           
-          
-            const thisCounter = likeCounters[i];
-            // leggo il valore attuale del contatore
-            const number = parseInt(thisCounter.innerHTML);
-            // incremento il valore del contatore 
-            thisCounter.innerHTML = number + 1;
-
-            // EXTRA: incrementare anche il numero di likes nell'array di oggetti
-            // anche questo poco elegante ma, per mantenere le cose semplici, utilizzando sempre lo stesso indice i
-            const likedPost = posts[i];
-            likedPost.likes++;
-        } else {
-            // EXTRA BONUS: possiamo togliere il like
-            element.classList.remove('like-button--liked');
-            // recupero il contatore associato a questo post tramite l'indice i
-            const thisCounter = likeCounters[i];
-            // leggo il valore attuale del contatore
-            const number = parseInt(thisCounter.innerHTML);
-            // decremento il valore del contatore
-            thisCounter.innerHTML = number - 1;
-
-            // EXTRA: decrementiamo il numero di like dall'array di oggetti
-            const likedPost = posts[i];
-            likedPost.likes--;
-        }
-    });
-}
+const buttonElement = document.querySelectorAll('js-like-button');
+console.log(buttonElement);
